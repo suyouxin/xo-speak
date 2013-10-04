088b2276640c530369517d0d34ba6bfc62ae4108
Aurelie Aubertin <aurelie@morphoss.com>
2013-10-04 15:55:37 +1300

    Add equations for the eyes inside for 1 to 5 eyes

--- src/com/morphoss/xo_speak/views/eyeInLayout.java
+++ src/com/morphoss/xo_speak/views/eyeInLayout.java
@@ -133,65 +133,182 @@ public class eyeInLayout extends View implements SurfaceHolder.Callback {
 	}
 
     public void calcEye(int touchX, int touchY) {
-    	int width = this.getMeasuredWidth()/2;
-		int height = this.getMeasuredHeight()/4;
-		int radius = width/10;
-    	
-		int X1 = width+height;
-		int Y1 = height;
-		int X2 = width-height;
-		int Y2 = height;
+    	int w = this.getMeasuredWidth()/2;
+		int h = this.getMeasuredHeight()/4;
+		int r = w/12;
+		int Y = h;
 		
-		if(Y1 < touchY){
-			Log.d(TAG,"Y1 < touchY");
-		double centerToPoint1 = Math.sqrt(Math.pow(touchX-X1, 2)+Math.pow(touchY-Y1, 2));
-		double centerToPoint2 = Math.sqrt(Math.pow(touchX-X2, 2)+Math.pow(touchY-Y2, 2));
-		int projX1 = (int) (X1 + ((radius*(touchX-X1))/centerToPoint1));
-		int projX2 = (int) (X2 + ((radius*(touchX-X2))/centerToPoint2));
-		int projY1 = (int) (Y1 + Math.sqrt(Math.pow(radius, 2) - Math.pow(projX1 - X1, 2)));
-		int projY2 = (int) (Y2 + Math.sqrt(Math.pow(radius, 2) - Math.pow(projX2 - X2, 2)));
-		mEye21 = projX1;
-		mEyeY = projY1;
-		mEye22 = projX2;
-		mEyeY = projY2;
+		int X1 = w;
+		int X21 = w+h;
+		int X22 = w-h;
+		int X31 = w-2*h;
+		int X32 = w;
+		int X33 = w+2*h;
+		int X41 = w-3*h;
+		int X42 = w-h;
+		int X43 = w+h;
+		int X44 = w+3*h;
+		int X51 = w-3*h;
+		int X52 = (int) (w-1.5*h);
+		int X53 = w;
+		int X54 = (int) (w+1.5*h);
+		int X55 = w+3*h;		
+		
+		if(Y < touchY){
+			Log.d(TAG,"Y < touchY");
+		double centerToPoint1 = Math.sqrt(Math.pow(touchX-X1, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint21 = Math.sqrt(Math.pow(touchX-X21, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint22 = Math.sqrt(Math.pow(touchX-X22, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint31 = Math.sqrt(Math.pow(touchX-X31, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint32 = Math.sqrt(Math.pow(touchX-X32, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint33 = Math.sqrt(Math.pow(touchX-X33, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint41 = Math.sqrt(Math.pow(touchX-X41, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint42 = Math.sqrt(Math.pow(touchX-X42, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint43 = Math.sqrt(Math.pow(touchX-X43, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint44 = Math.sqrt(Math.pow(touchX-X44, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint51 = Math.sqrt(Math.pow(touchX-X51, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint52 = Math.sqrt(Math.pow(touchX-X52, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint53 = Math.sqrt(Math.pow(touchX-X53, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint54 = Math.sqrt(Math.pow(touchX-X54, 2)+Math.pow(touchY-Y, 2));
+		double centerToPoint55 = Math.sqrt(Math.pow(touchX-X55, 2)+Math.pow(touchY-Y, 2));
+		
+		int projX1 = (int) (X1 + ((r*(touchX-X1))/centerToPoint1));
+		int projX21 = (int) (X21 + ((r*(touchX-X21))/centerToPoint21));
+		int projX22 = (int) (X22 + ((r*(touchX-X22))/centerToPoint22));
+		int projX31 = (int) (X31 + ((r*(touchX-X31))/centerToPoint31));
+		int projX32 = (int) (X32 + ((r*(touchX-X32))/centerToPoint32));
+		int projX33 = (int) (X33 + ((r*(touchX-X33))/centerToPoint33));
+		int projX41 = (int) (X41 + ((r*(touchX-X41))/centerToPoint41));
+		int projX42 = (int) (X42 + ((r*(touchX-X42))/centerToPoint42));
+		int projX43 = (int) (X43 + ((r*(touchX-X43))/centerToPoint43));
+		int projX44 = (int) (X44 + ((r*(touchX-X44))/centerToPoint44));
+		int projX51 = (int) (X51 + ((r*(touchX-X51))/centerToPoint51));
+		int projX52 = (int) (X52 + ((r*(touchX-X52))/centerToPoint52));
+		int projX53 = (int) (X53 + ((r*(touchX-X53))/centerToPoint53));
+		int projX54 = (int) (X54 + ((r*(touchX-X54))/centerToPoint54));
+		int projX55 = (int) (X55 + ((r*(touchX-X55))/centerToPoint55));
+		
+		int projY = (int) (Y + Math.sqrt(Math.pow(r, 2) - Math.pow(projX21 - X21, 2)));
+		
+		mEyeY = projY;
+		mEye1 = projX1;
+		mEye21 = projX21;
+		mEye22 = projX22;
+		mEye31 = projX31;
+		mEye32 = projX32;
+		mEye33 = projX33;
+		mEye41 = projX41;
+		mEye42 = projX42;
+		mEye43 = projX43;
+		mEye44 = projX44;
+		mEye51 = projX51;
+		mEye52 = projX52;
+		mEye53 = projX53;
+		mEye54 = projX54;
+		mEye55 = projX55;
+		
+		
+		
+		
 		}
-		if(Y1 > touchY){
-			Log.d(TAG,"Y1 > touchY, touchX > X1");
-			double centerToPoint3 = Math.sqrt(Math.pow(touchX-X1, 2)+Math.pow(Y1-touchY, 2));
-			double centerToPoint4 = Math.sqrt(Math.pow(touchX-X2, 2)+Math.pow(Y2-touchY, 2));
-			int projX3 = (int) (X1 + ((radius*(touchX-X1))/centerToPoint3));
-			int projX4 = (int) (X2 + ((radius*(touchX-X2))/centerToPoint4));
-			int projY3 = (int) (Y1 - Math.sqrt(Math.pow(radius, 2) - Math.pow(projX3 - X1, 2)));
-			int projY4 = (int) (Y2 - Math.sqrt(Math.pow(radius, 2) - Math.pow(projX4 - X2, 2)));
-			Log.d(TAG,"X3 : "+projX3+" Y3 : "+projY3);
-			Log.d(TAG,"X4 : "+projX4+" Y4 : "+projY4);
-			mEye21 = projX3;
-			mEyeY = projY3;
-			mEye22 = projX4;
-			mEyeY = projY4;
+		if(Y > touchY){
+			Log.d(TAG,"Y > touchY, touchX > X1");
+			double centerToPoint1a = Math.sqrt(Math.pow(touchX-X1, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint21a = Math.sqrt(Math.pow(touchX-X21, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint22a = Math.sqrt(Math.pow(touchX-X22, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint31a = Math.sqrt(Math.pow(touchX-X31, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint32a = Math.sqrt(Math.pow(touchX-X32, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint33a = Math.sqrt(Math.pow(touchX-X33, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint41a = Math.sqrt(Math.pow(touchX-X41, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint42a = Math.sqrt(Math.pow(touchX-X42, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint43a = Math.sqrt(Math.pow(touchX-X43, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint44a = Math.sqrt(Math.pow(touchX-X44, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint51a = Math.sqrt(Math.pow(touchX-X51, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint52a = Math.sqrt(Math.pow(touchX-X52, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint53a = Math.sqrt(Math.pow(touchX-X53, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint54a = Math.sqrt(Math.pow(touchX-X54, 2)+Math.pow(Y-touchY, 2));
+			double centerToPoint55a = Math.sqrt(Math.pow(touchX-X55, 2)+Math.pow(Y-touchY, 2));
+			
+			int projX1a = (int) (X1 + ((r*(touchX-X1))/centerToPoint1a));
+			int projX21a = (int) (X21 + ((r*(touchX-X21))/centerToPoint21a));
+			int projX22a = (int) (X22 + ((r*(touchX-X22))/centerToPoint22a));
+			int projX31a = (int) (X31 + ((r*(touchX-X31))/centerToPoint31a));
+			int projX32a = (int) (X32 + ((r*(touchX-X32))/centerToPoint32a));
+			int projX33a = (int) (X33 + ((r*(touchX-X33))/centerToPoint33a));
+			int projX41a = (int) (X41 + ((r*(touchX-X41))/centerToPoint41a));
+			int projX42a = (int) (X42 + ((r*(touchX-X42))/centerToPoint42a));
+			int projX43a = (int) (X43 + ((r*(touchX-X43))/centerToPoint43a));
+			int projX44a = (int) (X44 + ((r*(touchX-X44))/centerToPoint44a));
+			int projX51a = (int) (X51 + ((r*(touchX-X51))/centerToPoint51a));
+			int projX52a = (int) (X52 + ((r*(touchX-X52))/centerToPoint52a));
+			int projX53a = (int) (X53 + ((r*(touchX-X53))/centerToPoint53a));
+			int projX54a = (int) (X54 + ((r*(touchX-X54))/centerToPoint54a));
+			int projX55a = (int) (X55 + ((r*(touchX-X55))/centerToPoint55a));
+			
+			int projY = (int) (Y - Math.sqrt(Math.pow(r, 2) - Math.pow(projX21a - X21, 2)));
+			
+			mEyeY = projY;
+			mEye1 = projX1a;
+			mEye21 = projX21a;
+			mEye22 = projX22a;
+			mEye31 = projX31a;
+			mEye32 = projX32a;
+			mEye33 = projX33a;
+			mEye41 = projX41a;
+			mEye42 = projX42a;
+			mEye43 = projX43a;
+			mEye44 = projX44a;
+			mEye51 = projX51a;
+			mEye52 = projX52a;
+			mEye53 = projX53a;
+			mEye54 = projX54a;
+			mEye55 = projX55a;			
 		}
 		
     }
 	
     public void moveEye(double i) {
-    	int width = this.getMeasuredWidth()/2;
-		int height = this.getMeasuredHeight()/4;
-		int radius = width/8;
-    	
-		int X1 = width+height;
-		int Y1 = height;
-		int X2 = width-height;
-		int Y2 = height;
-		mEyeY = Y1;
-		mEyeY = Y2;
-		mEye21 = X1;
-		mEye22 = X2;
-			mEyeY += i;
-			mEyeY += i;
-			if(mEyeY >= Y1+radius || mEyeY >= Y2+radius){
-				mEyeY = Y1 - radius;
-				mEyeY = Y2 - radius;
+    	int w = this.getMeasuredWidth()/2;
+		int h = this.getMeasuredHeight()/4;
+		int r = w/12;
+		int Y = h;
+		
+		int X1 = w;
+		int X21 = w+h;
+		int X22 = w-h;
+		int X31 = w-2*h;
+		int X32 = w;
+		int X33 = w+2*h;
+		int X41 = w-3*h;
+		int X42 = w-h;
+		int X43 = w+h;
+		int X44 = w+3*h;
+		int X51 = w-3*h;
+		int X52 = (int) (w-1.5*h);
+		int X53 = w;
+		int X54 = (int) (w+1.5*h);
+		int X55 = w+3*h;	
+		
+		mEyeY = Y;
+		mEyeY += i;
+			if(mEyeY >= Y+r ){
+				mEyeY = Y - r;
 			}
+		mEye1 = X1;	
+		mEye21 = X21;
+		mEye22 = X22;
+		mEye31 = X31;
+		mEye32 = X32;
+		mEye33 = X33;
+		mEye41 = X41;
+		mEye42 = X42;
+		mEye43 = X43;
+		mEye44 = X44;
+		mEye51 = X51;
+		mEye52 = X52;
+		mEye53 = X53;
+		mEye54 = X54;
+		mEye55 = X55;
     }
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
